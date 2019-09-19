90f92c33d95b11332161a3389bb1c61a0b2bea08
SElioenai <44786318+SElioenai@users.noreply.github.com>
2019-09-19 16:36:48 -0300

    Skytef models

---
+++ src/main/java/skytef/fidelidade/App.java
@@ -0,0 +1,13 @@
+package skytef.fidelidade;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/skytef/fidelidade/model/Product.java
@@ -0,0 +1,43 @@
+package skytef.fidelidade.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+////import lombok.Getter;
+////import lombok.Setter;
+//
+//@Getter
+//@Setter
+
+@Entity @Table(name = "product")
+public class Product {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int product_id;
+	private String product_description;
+	private double product_price;
+	
+	public int getId_product() {
+		return product_id;
+	}
+	public void setId_product(int product_id) {
+		this.product_id = product_id;
+	}
+	public String getProduct_description() {
+		return product_description;
+	}
+	public void setProduct_description(String product_description) {
+		this.product_description = product_description;
+	}
+	public double getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(double product_price) {
+		this.product_price = product_price;
+	}
+	
+
+	
+}
